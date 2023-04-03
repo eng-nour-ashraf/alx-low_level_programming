93b5685805556bf38dff3e683efc9bd8ec5cc2f4
nour ashraf <devnourashraf123@gmail.com>
2023-04-03 03:23:26 -0700

    more more

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - function
+ * function that fills memory with a constant byte.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+	s[i] = b;
+	i++;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - function
+ * function that copies memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[];
+		i++;
+	}
+
+	return (dest);
+}
